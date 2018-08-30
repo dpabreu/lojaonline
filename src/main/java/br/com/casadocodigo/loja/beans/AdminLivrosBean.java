5bff1c5da5a761967e2d61737245682fd22d3949
Daniel Abreu <dpabreu@gmail.com>
2018-08-30 16:49:47 -0300

    Persistindo dados.
    Signed-off-by: Daniel Abreu <dpabreu@gmail.com>

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
+import br.com.casadocodigo.loja.daos.LivroDao;
 import br.com.casadocodigo.loja.models.Livro;
 
 @Named
@@ -10,6 +13,9 @@ import br.com.casadocodigo.loja.models.Livro;
 public class AdminLivrosBean {
 	
 	private Livro livro = new Livro();
+	
+	@Inject
+	private LivroDao livroDao;
 	
 	public Livro getLivro() {
 		return livro;
@@ -19,10 +25,9 @@ public class AdminLivrosBean {
 		this.livro = livro;
 	}
 
+	@Transactional
 	public void salvar() {
-
 		System.out.println("Livro cadastrado com sucesso." + livro);
-		
-		
+		livroDao.salvar(livro);
 	}
 }
